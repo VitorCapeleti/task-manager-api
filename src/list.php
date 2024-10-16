26bfdcbd3b76b4c1942f330182461d1d6cd7cd81
Vitor Capeleti <vitorcapeleti59@gmail.com>
2024-10-16 18:21:42 -0300

    Arquivos de update e teste de update

--- src/list.php
+++ src/list.php
@@ -5,11 +5,17 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Read</title>
-    <style>
-    </style>
+    <link rel="stylesheet" href="../src/css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
   <body class="p-3 mb-2 bg-primary-subtle text-primary-emphasis">
+  <div class="voltar">
+            <a href="index.php"><button>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-return-left" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M14.5 1.5a.5.5 0 0 1 .5.5v4.8a2.5 2.5 0 0 1-2.5 2.5H2.707l3.347 3.346a.5.5 0 0 1-.708.708l-4.2-4.2a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 8.3H12.5A1.5 1.5 0 0 0 14 6.8V2a.5.5 0 0 1 .5-.5"/>
+                </svg>
+            </button></a>
+        </div>
     <div class="m-5 ">
     <table class="table table-striped ">
         <thead>
@@ -17,6 +23,7 @@
             <th scope="col">#</th>
             <th scope="col">Nome</th>
             <th scope="col">Email</th>
+            <!--<th scope="col">Senha</th>-->
             <th scope="col">...</th>
             </tr>
         </thead>
@@ -30,8 +37,9 @@
                     echo"<th scope=\"col\">".$user_data['id_int_user']."</th>";
                     echo"<th>".$user_data['nome_var_user']."</th>";
                     echo"<th>".$user_data['email_var_user']."</th>";
+                    //echo"<th>".$user_data['password_var_user']."</th>";
                     echo"<th>
-                                                    <a class='btn btn-primary' href='#'>
+                                                    <a class='btn btn-primary' href='edit.php?id=$user_data[id_int_user]'>
                                 <svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-pencil\" viewBox=\"0 0 16 16\">
   <path d=\"M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325\"/>
                                 </svg>
@@ -45,6 +53,7 @@
         </tbody>
     </table>
     </div>
+
 
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
