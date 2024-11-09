d2fd0ff3d1ef6e6559c69a7ec0c9ed451357432f
Vitor Capeleti <vitorcapeleti59@gmail.com>
2024-11-09 12:37:49 -0300

    Teste novo

---
+++ src/insertTest.php
@@ -0,0 +1,20 @@
+<?php
+require_once '../src/connection.php';
+$connObj = new connectionClass();
+$connection = $connObj->ConectarBD();
+
+        $sql = "SELECT * FROM user";
+        $result = $connection->query($sql);
+
+
+if ($result) {
+    echo "Insert test passed.\n";
+    exit(0);
+} else {
+    echo "Insert test failed.\n";
+    exit(1);
+}
+
+
+
+?>
