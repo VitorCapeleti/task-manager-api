d161c2468e133d5e002566c7f0b8f90e9c641597
Vitor Capeleti <vitorcapeleti59@gmail.com>
2024-11-19 16:47:46 -0300

    corrigindo variaveis e css

--- src/list.php
+++ src/list.php
@@ -5,7 +5,98 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Read</title>
-    <link rel="stylesheet" href="../src/css/style.css">
+    <!--<link rel="stylesheet" href="../src/css/style.css">-->
+    <style type="text/css">
+        body{
+    background: rgb(9, 0, 134);
+    overflow-x: hidden;
+}
+.container{
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+.box{
+    width: 300px;
+    height: 350px;
+    background: #e6e6e6;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    border-radius: 10px;
+    box-shadow: 5px 5px 5px black;;
+}
+.elementstxt{
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+}
+.elementstxt label{
+    color: rgb(0, 26, 255);
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    font-size: 20px;
+}
+.elementstxt input{
+    border: none;
+    border-radius: 20px;
+    padding: 7px;
+}
+.elements{
+    padding: 10px;
+    display: flex;
+    flex-direction: column;
+}
+.elements input{
+    border: none;
+    border-radius: 20px;
+    font-size: 15px;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    background-color: rgb(43, 116, 226);
+    color: aliceblue;
+    font-weight: bolder;
+    padding: 15px;
+    cursor: pointer;
+}
+.read{
+    padding: 15px;
+}
+.read button{
+    border: none;
+    border-radius: 20px;
+    font-size: 15px;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    background-color: rgb(43, 116, 226);
+    color: aliceblue;
+    font-weight: bolder;
+    padding: 15px;
+    cursor: pointer;
+}
+.voltar{
+    padding: 0px;
+}
+.voltar button{
+    border: none;
+    border-radius: 20px;
+    font-size: 15px;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    background-color: rgb(43, 116, 226);
+    color: aliceblue;
+    font-weight: bolder;
+    padding: 15px;
+    cursor: pointer;
+}
+.box h1{
+    text-align: center;
+    color: rgb(43, 116, 226);
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
+
+    </style>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
   <body class="p-3 mb-2 bg-primary-subtle text-primary-emphasis">
@@ -40,12 +131,12 @@
             $value = $read->listarUsuario();
                 while($user_data = mysqli_fetch_assoc($value )){
                     echo"<tr>";
-                    echo"<th scope=\"col\">".$user_data['id_int_user']."</th>";
+                    echo"<th scope=\"col\">".$user_data['id']."</th>";
                     echo"<th>".$user_data['nome']."</th>";
                     echo"<th>".$user_data['email']."</th>";
                     //echo"<th>".$user_data['password_var_user']."</th>";
                     echo"<th style=\"text-align:center;\">
-                            <a class='btn btn-primary' href='edit.php?id=$user_data[id_int_user]'>
+                            <a class='btn btn-primary' href='edit.php?id=$user_data[id]'>
                                 <svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-pencil\" viewBox=\"0 0 16 16\">
                                 <path d=\"M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325\"/>
                                 </svg>
--- src/verificar.php
+++ src/verificar.php
@@ -4,7 +4,97 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edit</title>
-    <link rel="stylesheet" href="../src/css/style.css">
+    <style type="text/css">
+        body{
+    background: rgb(9, 0, 134);
+    overflow-x: hidden;
+}
+.container{
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+.box{
+    width: 300px;
+    height: 350px;
+    background: #e6e6e6;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    border-radius: 10px;
+    box-shadow: 5px 5px 5px black;;
+}
+.elementstxt{
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+}
+.elementstxt label{
+    color: rgb(0, 26, 255);
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    font-size: 20px;
+}
+.elementstxt input{
+    border: none;
+    border-radius: 20px;
+    padding: 7px;
+}
+.elements{
+    padding: 10px;
+    display: flex;
+    flex-direction: column;
+}
+.elements input{
+    border: none;
+    border-radius: 20px;
+    font-size: 15px;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    background-color: rgb(43, 116, 226);
+    color: aliceblue;
+    font-weight: bolder;
+    padding: 15px;
+    cursor: pointer;
+}
+.read{
+    padding: 15px;
+}
+.read button{
+    border: none;
+    border-radius: 20px;
+    font-size: 15px;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    background-color: rgb(43, 116, 226);
+    color: aliceblue;
+    font-weight: bolder;
+    padding: 15px;
+    cursor: pointer;
+}
+.voltar{
+    padding: 0px;
+}
+.voltar button{
+    border: none;
+    border-radius: 20px;
+    font-size: 15px;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    background-color: rgb(43, 116, 226);
+    color: aliceblue;
+    font-weight: bolder;
+    padding: 15px;
+    cursor: pointer;
+}
+.box h1{
+    text-align: center;
+    color: rgb(43, 116, 226);
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
+
+    </style>
 </head>
 <body>
 <?php
